dabcd80f09bd8036a66732f4317de8f5411ff277
Edwin Ligorio Salgado <lesf@miflink.com>
2017-10-02 20:21:18 -0500

    agregando .env

--- goodreads/settings/production.py
+++ goodreads/settings/production.py
@@ -7,10 +7,20 @@ DEBUG = False
 ALLOWED_HOSTS = ['*']
 
 
-DATABASES = dict()
+DATABASES = {'default': {'ENGINE': 'django.db.backends.postgresql_psycopg2',
+                         'NAME': os.getenv("DB_NAME"),
+                         'USER': os.getenv("DB_USER"),
+                         'PASSWORD': os.getenv("DB_PASSWORD"),
+                         'HOST': 'localhost',
+                         'PORT': '5432'
+                         }
+             }
 
-DATABASES['default'] = dj_database_url.config()  # Heroku config
 
-SECURE_PROXY_SSL_HEADER = ('HTTP_X_FORWARDED_PROTO', 'https')
+#DATABASES = dict()
+
+# DATABASES['default'] = dj_database_url.config()  # Heroku config
+
+#SECURE_PROXY_SSL_HEADER = ('HTTP_X_FORWARDED_PROTO', 'https')
 
 STATIC_ROOT = os.path.join(os.getcwd(), 'static')
--- manage.py
+++ manage.py
@@ -2,6 +2,15 @@
 import os
 import sys
 
+from dotenv import load_dotenv
+
+try:
+
+    dotenv_path = os.path.join(os.path.dirname(__file__), '.env')
+    load_dotenv(dotenv_path)
+except:
+    pass
+
 if __name__ == "__main__":
     os.environ.setdefault("DJANGO_SETTINGS_MODULE", "goodreads.settings.local")
     try:
