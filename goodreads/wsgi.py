650fe9336f11a695f4dfe17460174361ac92fecd
Edwin Ligorio Salgado <lesf@miflink.com>
2017-10-02 20:21:18 -0500

    agregando .env

--- goodreads/wsgi.py
+++ goodreads/wsgi.py
@@ -10,6 +10,12 @@ https://docs.djangoproject.com/en/1.11/howto/deployment/wsgi/
 import os
 
 from django.core.wsgi import get_wsgi_application
+from dotenv import load_dotenv
+	try:
+    dotenv_path = os.path.join(os.path.dirname(os.path.dirname(os.path.abspath(__file__))), '.env')
+    load_dotenv(dotenv_path)
+except:
+    pass
 
 os.environ.setdefault("DJANGO_SETTINGS_MODULE", "goodreads.settings")
 
